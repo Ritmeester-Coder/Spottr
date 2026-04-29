e38051ca4be95b30b3ec6026d752067dcefb8ade
Ritmeester-Coder <ritmeester@atomicmail.io>
2026-04-29 16:24:31 +0200

    fix firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,11 +3,16 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
+  apiKey: "AIzaSyDROaMZ_6x0KC7sllo0IhUKb1JjuWCObZc",
+  authDomain: "spottr-6d8a9.firebaseapp.com",
   projectId: "spottr-6d8a9",
+  storageBucket: "spottr-6d8a9.firebasestorage.app",
+  messagingSenderId: "502907666954",
+  appId: "1:502907666954:web:e9df3275df23ec812615f3",
+  measurementId: "G-HLEK6BZYL6"
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
 export const db = getFirestore(app);
+export const auth = getAuth(app);
